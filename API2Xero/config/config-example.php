273f9d480fca663fdc97747151d4156f39018cac
Adam Prescott <adam.prescott@realitymine.com>
2015-12-12 16:16:31 +0000

    Added somee config and description to index.php

--- API2Xero/config/config-example.php
+++ API2Xero/config/config-example.php
@@ -1,1 +1,8 @@
 <?php
+define('BASE_PATH', dirname(__FILE__) );
+define("XRO_APP_TYPE", "Private");
+define("OAUTH_CALLBACK", "oob");
+define("XERO_CONSUMER_KEY", '');
+define("XERO_CONSUMER_SECRET", '');
+define("XERO_CERTIFICATE", BASE_PATH . '/config/certs/xero.cer');
+define("XERO_PRIVATE_KEY", BASE_PATH . '/config/certs/xero.pem');
--- API2Xero/index.php
+++ API2Xero/index.php
@@ -1,7 +1,15 @@
 <?php
+require_once 'config/config.php';
+require_once 'libs/XeroOAuth-PHP/lib/XeroOAuth.php';
 
 switch($_GET['method']) {
     case 'makepurchase':
-        
+        /* This will connect to a SQLite Database, insert a row with datetime of
+        purchase with value. Then make a request to Xero.
+
+        Upon success, it will return a HTTP 201 code along with whatever content
+        is posted to it. On Failure it wil return a HTTP 500 with an error message
+        */
+
     break;
 }
