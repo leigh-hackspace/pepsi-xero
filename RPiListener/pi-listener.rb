1c37441b111ffd96338a223fea7ca4bb7952d087
pacharanero <marcusbaw@gmail.com>
2015-12-13 12:28:14 +0000

    created environment file containing Xero credentials, added this to .gitignore list, created ruby version of xero interface

--- RPiListener/pi-listener.rb
+++ RPiListener/pi-listener.rb
@@ -1,5 +1,9 @@
 require 'pi_piper'
+require 'xero_gateway'
+
 include PiPiper
+
+gateway = XeroGateway::Gateway.new(YOUR_OAUTH_CONSUMER_KEY, YOUR_OAUTH_CONSUMER_SECRET)
 
 after :pin => 23, :goes => :high do
   puts "can vended"
---
+++ RPiListener/xero-connector-test.rb
@@ -0,0 +1,2 @@
+require 'xero_gateway'
+gateway = XeroGateway::Gateway.new(YOUR_OAUTH_CONSUMER_KEY, YOUR_OAUTH_CONSUMER_SECRET)
