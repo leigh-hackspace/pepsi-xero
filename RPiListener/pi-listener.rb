d0cfced75daee127bb079f8eca79333d2081ea5e
pacharanero <marcusbaw@gmail.com>
2015-12-13 16:06:30 +0000

    large commit of everything :-|

--- RPiListener/pi-listener.rb
+++ RPiListener/pi-listener.rb
@@ -1,12 +1,33 @@
 require 'pi_piper'
-require 'xero_gateway'
+require 'xeroizer'
 
 include PiPiper
 
-gateway = XeroGateway::Gateway.new(YOUR_OAUTH_CONSUMER_KEY, YOUR_OAUTH_CONSUMER_SECRET)
+def setup
+	#setup connection
+	@client = Xeroizer::PrivateApplication.new(ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET'], ENV['PATH_TO_PRIVATE_KEY'])
+end
+
+def send_to_xero
+	#get info about where to send the transaction
+	@contact = @client.Contact.find('8ba5474b-fd18-4d0d-8a8c-cc5ad23ffeec') #gets the pepsi machine as a contact
+	@item = client.Item.find('27b832f8-f2db-407c-b654-2ab861052dba') #gets the line item that means one drinks can
+	@lineitem = Xeroizer::Record::LineItem.new(@item)
+
+	#start composing the invoice
+	@invoice = @client.Invoice.build
+	@invoice.contact = @contact
+	@invoice.type = 'ACCREC'
+	@invoice.date = Time.now
+	@invoice.due_date = Time.now
+	@invoice.status = 'DRAFT'
+	@invoice.invoice_number = 'INV-1011'
+	@invoice.line_amount_types = 'Inclusive'
+	@invoice.add_lineitem(@lineitem)
+end
 
 after :pin => 23, :goes => :high do
-  puts "can vended"
+  send_to_xero
 end
 
 PiPiper.wait
