a7bcf5d3afb7bd322f029103c7b6feddd0da7b0f
pacharanero <marcusbaw@gmail.com>
2015-12-14 07:52:10 +0000

    work on auto-increment of invoice number from the last invoice in the system

--- rpi-gpio-to-xero.rb
+++ rpi-gpio-to-xero.rb
@@ -10,9 +10,10 @@ def setup
 end
 
 def send_to_xero
-	#get info about where to send the transaction
+	#get info about where to send the transaction and what to send
 	@contact = @client.Contact.find('8ba5474b-fd18-4d0d-8a8c-cc5ad23ffeec') #gets the pepsi machine as a contact
 	@item = @client.Item.find('27b832f8-f2db-407c-b654-2ab861052dba') #gets the line item that means one drinks can
+	@last_invoice_number = client.Invoice.all(:order => 'Date').last.invoice_number # get the last invoice in the system, we want to increment its invoice number
 
 	#start composing the invoice
 	@invoice = @client.Invoice.build
@@ -21,10 +22,14 @@ def send_to_xero
 	@invoice.date = Time.now
 	@invoice.due_date = Time.now
 	@invoice.status = 'DRAFT'
-	@invoice.invoice_number = 'INV-1014'
+	@invoice.invoice_number = @next_invoice_number
 	@invoice.line_amount_types = 'Inclusive'
 	@invoice.add_line_item(:item_code => 'beverage_vend')
 	@invoice.save
+end
+
+def next_invoice_number(@last_invoice_number)
+	@next_invoice_number = @last_invoice_number.match(/\d+$/).to_i+1 
 end
 
 after :pin => 23, :goes => :high do
