4b4ab39b49d85b5f3f6587e9f595984cd9a11278
pacharanero <marcusbaw@gmail.com>
2016-01-11 15:14:12 +0000

    bug hunting

--- rpi-gpio-to-xero.rb
+++ rpi-gpio-to-xero.rb
@@ -5,37 +5,37 @@ include PiPiper
 
 #setup connection
 def setup
-  @client = Xeroizer::PrivateApplication.new(ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET'], ENV['PATH_TO_PRIVATE_KEY'])
+  client = Xeroizer::PrivateApplication.new(ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET'], ENV['PATH_TO_PRIVATE_KEY'])
 end
 
-def send_to_xero(@client)
+def send_to_xero(client)
   #get info about where to send the transaction and what to send
-  contact = @client.Contact.find('8ba5474b-fd18-4d0d-8a8c-cc5ad23ffeec') #gets the pepsi machine as a contact
-  item = @client.Item.find('27b832f8-f2db-407c-b654-2ab861052dba') #gets the line item that means one drinks can
-  @last_invoice_number = @client.Invoice.all(:order => 'Date').last.invoice_number # get the last invoice in the system, we want to increment its invoice number  
+  contact = client.Contact.find('8ba5474b-fd18-4d0d-8a8c-cc5ad23ffeec') #gets the pepsi machine as a contact
+  item = client.Item.find('27b832f8-f2db-407c-b654-2ab861052dba') #gets the line item that means one drinks can
+  last_invoice_number = client.Invoice.all(:order => 'Date').last.invoice_number # get the last invoice in the system, we want to increment its invoice number  
 
   #start composing the invoice
-  invoice = @client.Invoice.build
+  invoice = client.Invoice.build
   invoice.contact = contact
   invoice.type = 'ACCREC'
   invoice.date = Time.now
   invoice.due_date = Time.now
   invoice.status = 'AUTHORISED'
-  invoice.invoice_number = @next_invoice_number
+  invoice.invoice_number = next_invoice_number(last_invoice_number)
   invoice.line_amount_types = 'Inclusive'
   invoice.add_line_item(:item_code => 'beverage_vend')
   invoice.save
 end
 
-def next_invoice_number
+def next_invoice_number(last_invoice_number)
   #matches the numeric part of the invoice number, increments it, and makes it back into the right format for Xero.
-  @next_invoice_number = "INV-"+(@last_invoice_number.match(/\d+$/).to_s.to_i+1).to_s
+  next_invoice_number = "INV-"+(last_invoice_number.match(/\d+$/).to_s.to_i+1).to_s
 end
 
 setup
 
 after :pin => 23, :goes => :high do
-  send_to_xero(@client)
+  send_to_xero(client)
 end
 
 PiPiper.wait
